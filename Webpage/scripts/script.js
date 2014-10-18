c9f0bbcba56c07b56b2f57dfec6f6ae29037f051
David Duong <david@duong.cz>
2014-10-18 14:43:45 +0200

    Fixing search

--- Webpage/scripts/script.js
+++ Webpage/scripts/script.js
@@ -11,14 +11,31 @@ $(document).ready(function() {
 			if((current >= Math.max(nav, $(".hero.background").outerHeight())) || $(".nav").css("position") == "fixed") {
 				$(".nav").css("position", "fixed").addClass("floating");
 
-				if(diff <= 0) {
+				
+				if(diff <= 0 && -parseInt($(".nav").css("top")) >= nav) {
 					$(".nav").animate({"top": (delta = 0)+"px"}, 200);
-				} else {
-					$(".nav").stop(true).css("top", delta+"px");
+					
+				} else if (!$(".nav").is(":animated")) {
+					$(".nav").css("top", delta+"px");
+
+					if(delta <= -nav) {
+						$(this).val("");
+						$(".nav").removeClass("search-active");
+					}
 				}
 			} else {
 				$(".nav").css("position", "absolute");
 			}
 		}
+	});
+
+	$(".nav .search .icon").click(function(e) {
+		e.preventDefault();
+		$(".nav").addClass("search-active");
+		$(".nav .search input").focus();
+	});
+	$(".nav .search input").focusout(function(e) {
+		$(this).val("");
+		$(".nav").removeClass("search-active");
 	});
 });
