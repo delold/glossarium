8d29cb5f036942eff1c897188c3cef4a9de683bb
David Duong <david@duong.cz>
2015-02-25 18:05:18 +0100

    Fixing main layout

--- Webpage/scripts/script.js
+++ Webpage/scripts/script.js
@@ -40,6 +40,18 @@ $(document).ready(function() {
 		$(".nav").removeClass("search-active");
 	});
 
+	$(".nav .sidebar > ul > li > a").click(function(e) {
+		var element = $(this).parent();
+
+		if(element.hasClass("active")) {
+			$(".sidenav", element).animate({"min-height": 0}, 300);
+			element.removeClass("active");
+		} else {
+			$(".sidenav", element).animate({"min-height": element.height()*$(".sidenav > ul > li", element).length}, 300);
+			element.addClass("active");
+		}
+	});
+
 	$(".sidenav > ul > li").hover(function() {
 		if($(this).has("ul").length) {
 			$(this).closest(".sidenav").toggleClass("active");
@@ -48,7 +60,54 @@ $(document).ready(function() {
 		}
 	});
 
-	$("main img").click(function() {
-		console.log("AHOJ");
+	$(".article-list article a").click(function(e) {
+		if(!$(this).parent().is(".meta")) {
+			// e.preventDefault();
+		}
+	});
+
+	// $(document).mousemove(function() {
+		
+	// });
+
+	// $("main img").click(function() {
+	// 	console.log("AHOJ");
+	// });
+
+	function toggleNav(apply) {
+
+		if(apply === true) {
+			$("body").addClass("navon");
+
+			$(".page").animate({"left":"-" + Math.min($(document).width() * 0.85, 400) + "px"});
+			$(".nav .sidebar").animate({"right": 0});
+		} else if (apply === false) {
+			$("body").removeClass("navon");
+			$(".page").animate({"left":0});
+			$(".nav .sidebar").animate({"right":"-" + Math.min($(document).width() * 0.85, 400) + "px"});
+		} else {
+			toggleNav(($("body").hasClass("navon")) == false);
+		}
+	}
+
+	$(".nav .menu-open").click(function(event) {
+		event.stopPropagation();
+		toggleNav();
+	});
+
+	$(".nav .menu-close").click(function(event) {
+		toggleNav(false);
+	});
+
+	$(".nav .sidebar").click(function(event) {
+		if($("body").hasClass("navon")) {
+			event.stopPropagation();
+		}
+	});
+
+	$("body").click(function() {
+		if($("body").hasClass("navon")) {
+			toggleNav(false);
+		}
 	});
 });
